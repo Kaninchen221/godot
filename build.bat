da544734d946916083fd21e4b4a2e9be5a919aea
Kaninchen221 <pawel8krolik@gmail.com>
2025-01-17 16:36:13 +0100

    Add build and clean script files

---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+
+D:\Programy\Python_3_11_4\Scripts\scons debug_symbols=yes optimize=debug use_mingw=no dev_mode=yes vsproj=yes arch=x86_64
---
+++ clean.bat
@@ -0,0 +1,4 @@
+@echo off
+
+echo D:\Programy\Python_3_11_4\Scripts\scons --clean debug_symbols=yes optimize=debug use_mingw=no dev_mode=yes vsproj=yes arch=x86_64
+D:\Programy\Python_3_11_4\Scripts\scons -c
