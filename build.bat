66181e66c3f884d6f73fe26df7445ebc5999e29a
Kaninchen221 <pawel8krolik@gmail.com>
2025-01-17 16:41:40 +0100

    Update build and clean

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @echo off
 
 D:\Programy\Python_3_11_4\Scripts\scons debug_symbols=yes optimize=debug use_mingw=no dev_mode=yes vsproj=yes arch=x86_64
+pause
--- clean.bat
+++ clean.bat
@@ -2,3 +2,4 @@
 
 echo D:\Programy\Python_3_11_4\Scripts\scons --clean debug_symbols=yes optimize=debug use_mingw=no dev_mode=yes vsproj=yes arch=x86_64
 D:\Programy\Python_3_11_4\Scripts\scons -c
+pause
